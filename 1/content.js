80625ed853ca3c0e53f5f45a5fc839b8bde03169
Yash Jain <akshat0@yahoo.com>
2024-11-30 08:35:40 +0530

    clean

--- 1/content.js
+++
@@ -1,9 +0,0 @@
-// Create a sidebar div
-const sidebar = document.createElement('div');
-sidebar.id = 'extension-sidebar';
-sidebar.innerHTML = `
-  <h3>My Extension Sidebar</h3>
-  <p>This is a 20%-width sidebar.</p>
-`;
-
-document.body.appendChild(sidebar);
